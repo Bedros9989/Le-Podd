bb3f99c6e87697adcf46e37e96d23a7dcd0c9d64
sofierust <sofierust@gmail.com>
2023-10-24 14:01:47 +0200

    Snyggt

--- PodcastHanteraren/Form1.Designer.cs
+++ PodcastHanteraren/Form1.Designer.cs
@@ -54,6 +54,8 @@
             this.spara = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.spara2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.poddTabell)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.avsnittsTabell)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.kategoriTabell)).BeginInit();
@@ -70,6 +72,7 @@
             // 
             // läggTill
             // 
+            this.läggTill.BackColor = System.Drawing.Color.LightBlue;
             this.läggTill.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.läggTill.Location = new System.Drawing.Point(410, 71);
             this.läggTill.Margin = new System.Windows.Forms.Padding(2);
@@ -77,7 +80,7 @@
             this.läggTill.Size = new System.Drawing.Size(81, 24);
             this.läggTill.TabIndex = 1;
             this.läggTill.Text = "Lägg till";
-            this.läggTill.UseVisualStyleBackColor = true;
+            this.läggTill.UseVisualStyleBackColor = false;
             this.läggTill.Click += new System.EventHandler(this.button1_Click);
             // 
             // poddTabell
@@ -134,13 +137,14 @@
             // 
             // taBort
             // 
+            this.taBort.BackColor = System.Drawing.Color.LightBlue;
             this.taBort.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.taBort.Location = new System.Drawing.Point(410, 100);
             this.taBort.Name = "taBort";
             this.taBort.Size = new System.Drawing.Size(81, 23);
             this.taBort.TabIndex = 5;
             this.taBort.Text = "Ta bort";
-            this.taBort.UseVisualStyleBackColor = true;
+            this.taBort.UseVisualStyleBackColor = false;
             this.taBort.Click += new System.EventHandler(this.button2_Click);
             // 
             // urlLabel
@@ -195,13 +199,14 @@
             // 
             // ändra
             // 
+            this.ändra.BackColor = System.Drawing.Color.LightBlue;
             this.ändra.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ändra.Location = new System.Drawing.Point(410, 127);
             this.ändra.Name = "ändra";
             this.ändra.Size = new System.Drawing.Size(81, 23);
             this.ändra.TabIndex = 11;
             this.ändra.Text = "Ändra";
-            this.ändra.UseVisualStyleBackColor = true;
+            this.ändra.UseVisualStyleBackColor = false;
             this.ändra.Click += new System.EventHandler(this.ändra_Click);
             // 
             // filtrera
@@ -217,22 +222,23 @@
             // 
             // återställ
             // 
+            this.återställ.BackColor = System.Drawing.Color.LightBlue;
             this.återställ.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.återställ.Location = new System.Drawing.Point(276, 140);
             this.återställ.Name = "återställ";
             this.återställ.Size = new System.Drawing.Size(92, 23);
             this.återställ.TabIndex = 13;
             this.återställ.Text = "Återställ";
-            this.återställ.UseVisualStyleBackColor = true;
+            this.återställ.UseVisualStyleBackColor = false;
             this.återställ.Click += new System.EventHandler(this.återställ_Click);
             // 
             // avsnittLabel
             // 
             this.avsnittLabel.AutoSize = true;
-            this.avsnittLabel.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.avsnittLabel.Location = new System.Drawing.Point(643, 121);
+            this.avsnittLabel.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.avsnittLabel.Location = new System.Drawing.Point(663, 142);
             this.avsnittLabel.Name = "avsnittLabel";
-            this.avsnittLabel.Size = new System.Drawing.Size(83, 27);
+            this.avsnittLabel.Size = new System.Drawing.Size(66, 22);
             this.avsnittLabel.TabIndex = 16;
             this.avsnittLabel.Text = "Avsnitt";
             // 
@@ -240,11 +246,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(952, 349);
+            this.label1.Location = new System.Drawing.Point(914, 359);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 22);
+            this.label1.Size = new System.Drawing.Size(189, 22);
             this.label1.TabIndex = 17;
-            this.label1.Text = "Beskrivning";
+            this.label1.Text = "Beskrivning av avsnitt";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // kategoriTextBox
             // 
@@ -256,35 +263,38 @@
             // 
             // läggTill2
             // 
+            this.läggTill2.BackColor = System.Drawing.Color.LightBlue;
             this.läggTill2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.läggTill2.Location = new System.Drawing.Point(884, 144);
             this.läggTill2.Name = "läggTill2";
             this.läggTill2.Size = new System.Drawing.Size(75, 24);
             this.läggTill2.TabIndex = 19;
             this.läggTill2.Text = "Lägg till";
-            this.läggTill2.UseVisualStyleBackColor = true;
+            this.läggTill2.UseVisualStyleBackColor = false;
             this.läggTill2.Click += new System.EventHandler(this.läggTill2_Click);
             // 
             // ändra2
             // 
+            this.ändra2.BackColor = System.Drawing.Color.LightBlue;
             this.ändra2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ändra2.Location = new System.Drawing.Point(1056, 144);
             this.ändra2.Name = "ändra2";
             this.ändra2.Size = new System.Drawing.Size(75, 25);
             this.ändra2.TabIndex = 20;
             this.ändra2.Text = "Ändra";
-            this.ändra2.UseVisualStyleBackColor = true;
+            this.ändra2.UseVisualStyleBackColor = false;
             this.ändra2.Click += new System.EventHandler(this.ändra2_Click);
             // 
             // taBort2
             // 
+            this.taBort2.BackColor = System.Drawing.Color.LightBlue;
             this.taBort2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.taBort2.Location = new System.Drawing.Point(971, 144);
             this.taBort2.Name = "taBort2";
             this.taBort2.Size = new System.Drawing.Size(75, 25);
             this.taBort2.TabIndex = 21;
             this.taBort2.Text = "Ta bort";
-            this.taBort2.UseVisualStyleBackColor = true;
+            this.taBort2.UseVisualStyleBackColor = false;
             this.taBort2.Click += new System.EventHandler(this.taBort2_Click);
             // 
             // kategoriLabel2
@@ -315,17 +325,19 @@
             // 
             // spara
             // 
+            this.spara.BackColor = System.Drawing.Color.LightCyan;
             this.spara.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.spara.Location = new System.Drawing.Point(410, 127);
             this.spara.Name = "spara";
             this.spara.Size = new System.Drawing.Size(81, 23);
             this.spara.TabIndex = 24;
             this.spara.Text = "Spara";
-            this.spara.UseVisualStyleBackColor = true;
+            this.spara.UseVisualStyleBackColor = false;
             this.spara.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // richTextBox1
             // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
             this.richTextBox1.Location = new System.Drawing.Point(884, 386);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
@@ -335,14 +347,36 @@
             // 
             // spara2
             // 
+            this.spara2.BackColor = System.Drawing.Color.LightCyan;
             this.spara2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.spara2.Location = new System.Drawing.Point(1056, 144);
             this.spara2.Name = "spara2";
             this.spara2.Size = new System.Drawing.Size(75, 25);
             this.spara2.TabIndex = 26;
             this.spara2.Text = "Spara";
-            this.spara2.UseVisualStyleBackColor = true;
+            this.spara2.UseVisualStyleBackColor = false;
             this.spara2.Click += new System.EventHandler(this.spara2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(194, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 22);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Lägg till podcast";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(938, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 22);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Lägg till kategori";
             // 
             // Form1
             // 
@@ -350,6 +384,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SeaShell;
             this.ClientSize = new System.Drawing.Size(1171, 548);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.spara2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.spara);
@@ -416,6 +452,8 @@
         private System.Windows.Forms.Button spara;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button spara2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- PodcastHanteraren/Form1.cs
+++ PodcastHanteraren/Form1.cs
@@ -383,5 +383,15 @@ namespace PodcastHanteraren
                 }
             }
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
